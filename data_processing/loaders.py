1b16aaa0611b12217eb9acff36d2e4b43754a03f
PRINCEPALSINGH DEORA <work.princedeora@gmail.com>
2026-07-31 18:55:15 +0530

    feat: implement IQ loaders for drone and wireless RF datasets

--- data_processing/loaders.py
+++ data_processing/loaders.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def load_bin(path) -> np.ndarray:
     """
-    Load binary data from a file and return the real and imaginary parts of the signal.
+    Load binary data from a file and return IQ signals.
     """
     raw_data = np.fromfile(path, dtype=np.int16)
 
@@ -13,3 +13,18 @@ def load_bin(path) -> np.ndarray:
     iq = I + 1j * Q
 
     return iq
+
+def load_data(path) -> np.ndarray:
+    """
+    Load data from a .data file and return IQ signals.
+    """
+    raw_data = np.fromfile(path, dtype=np.int16)
+
+    # convert to complex numbers
+    I = raw_data[0::2].astype(np.float32)
+    Q = raw_data[1::2].astype(np.float32)
+
+    iq = I + 1j * Q
+
+    return iq
+
