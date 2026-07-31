f8d0ef29a8d99b4982ce65012653878638b4c5a6
PRINCEPALSINGH DEORA <work.princedeora@gmail.com>
2026-07-31 21:35:35 +0530

    feat: implement windowed dataset creation pipeline

---
+++ data_processing/save.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+import numpy as np
+from data_processing.loaders import load_bin
+from data_processing.preprocessing import create_windows
+from data_processing.split import split_dataset
+
+
+def process_split(
+    files: list[Path],
+    output_dir: Path,
+):
+    """Load recordings, create windows, and save them."""
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for file in files:
+        iq = load_bin(str(file))
+        windows = create_windows(iq)
+        output_path = output_dir / f"{file.stem}.npy"
+        np.save(output_path, windows)
+
+
+def create_dataset(
+    drone_path: str,
+    non_drone_path: str,
+    output_dir: str,
+):
+    """Create train, validation, and test datasets."""
+
+    drone_files = [
+        file
+        for file in Path(drone_path).glob("*.bin")
+        if file.is_file()
+    ]
+
+    non_drone_files = [
+        file
+        for file in Path(non_drone_path).rglob("*.data")
+        if file.is_file()
+    ]
+
+    (train_drone, val_drone, test_drone, train_non_drone, val_non_drone, test_non_drone,) = split_dataset(drone_files, non_drone_files,)
+    output_dir = Path(output_dir)
+
+    process_split(
+        train_drone,
+        output_dir / "train" / "drone",
+    )
+
+    process_split(
+        train_non_drone,
+        output_dir / "train" / "non_drone",
+    )
+
+    process_split(
+        val_drone,
+        output_dir / "validation" / "drone",
+    )
+
+    process_split(
+        val_non_drone,
+        output_dir / "validation" / "non_drone",
+    )
+
+    process_split(
+        test_drone,
+        output_dir / "test" / "drone",
+    )
+
+    process_split(
+        test_non_drone,
+        output_dir / "test" / "non_drone",
+    )
