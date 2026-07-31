b10a2265d36344deb05fd2229322eeaa0bb2db91
PRINCEPALSINGH DEORA <work.princedeora@gmail.com>
2026-08-01 00:57:00 +0530

    refactor: handled some effors

--- data_processing/loaders.py
+++ data_processing/loaders.py
@@ -6,6 +6,11 @@ def load_bin(path) -> np.ndarray:
     """
     raw_data = np.fromfile(path, dtype=np.int16)
 
+    if len(raw_data) % 2 != 0:
+        raise ValueError(
+            f"{path} does not contain an even number of float32 values."
+        )
+
     # convert to complex numbers
     I = raw_data[0::2].astype(np.float32)
     Q = raw_data[1::2].astype(np.float32)
